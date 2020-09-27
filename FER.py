8019861ddb03c1dbe1d7efb6725ede9dc0d94eff
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-09-27 12:27:46 -0700

    loaded in libraries

---
+++ FER.py
@@ -0,0 +1,10 @@
+#Import libraries
+import numpy as np
+import cv2
+
+from keras.emotion_models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+from keras.layers import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
