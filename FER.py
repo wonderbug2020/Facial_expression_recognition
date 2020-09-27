6aacc5296fd153a1c1c303644aaac33e6bfec73b
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-09-27 13:43:23 -0700

    build the models

--- FER.py
+++ FER.py
@@ -27,3 +27,19 @@ validation_generator = val_datagen.flow_from_directory(
         batch_size=64,
         color_mode="gray_framescale",
         class_mode='categorical')
+
+#Build the models
+emotion_model = Sequential()
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
