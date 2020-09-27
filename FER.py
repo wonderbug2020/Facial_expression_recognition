0b2daa97b7e424e7561b5990f2f3986f1504a97b
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-09-27 13:38:45 -0700

    setup directories and generators

--- FER.py
+++ FER.py
@@ -8,3 +8,22 @@ from keras.layers import Conv2D
 from keras.optimizers import Adam
 from keras.layers import MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
+
+#Load in the data and create the generators
+train_dir = 'data/train'
+val_dir = 'data/test'
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="gray_framescale",
+        class_mode='categorical')
+validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="gray_framescale",
+        class_mode='categorical')
